70642b3e46766468aa1dba9904f313d67449b386
OmurbekovTemirlan <Japarhanovaldik@gmail.com>
2024-02-20 00:33:38 +0600

    Initial Commit

---
+++ 4 month 3 lesson dz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4 month 3 lesson dz
+//
+//  Created by Apple on 20.2.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
